2294bc98661904f536260d806af5a4039fdd1b2d
Leechael Yim <yanleech@gmail.com>
2026-05-16 03:20:37 +0800

    test: add Decimal canonical precision reflection tests

--- tests/types/test_decimal_variants.py
+++ tests/types/test_decimal_variants.py
@@ -1,7 +1,10 @@
+from unittest import TestCase
+
 from sqlalchemy import Column
 from sqlalchemy.sql.ddl import CreateTable
 
 from clickhouse_sqlalchemy import types, engines, Table
+from clickhouse_sqlalchemy.drivers.base import ClickHouseDialect
 from tests.testcase import CompilationTestCase
 
 
@@ -63,3 +66,33 @@ class Decimal256CompilationTestCase(CompilationTestCase):
             'CREATE TABLE test_decimal256 '
             '(x Decimal256(8)) ENGINE = Memory'
         )
+
+
+class DecimalVariantReflectionTestCase(TestCase):
+    def setUp(self):
+        self.dialect = ClickHouseDialect()
+
+    def _assert_reflects_canonical_decimal(self, spec, expected_precision, expected_scale):
+        coltype = self.dialect._get_column_type('x', spec)
+
+        self.assertIsInstance(coltype, types.Decimal)
+        self.assertNotIsInstance(coltype, (
+            types.Decimal32,
+            types.Decimal64,
+            types.Decimal128,
+            types.Decimal256,
+        ))
+        self.assertEqual(coltype.precision, expected_precision)
+        self.assertEqual(coltype.scale, expected_scale)
+
+    def test_reflect_decimal32_as_clickhouse_canonical_precision(self):
+        self._assert_reflects_canonical_decimal('Decimal(9, 2)', 9, 2)
+
+    def test_reflect_decimal64_as_clickhouse_canonical_precision(self):
+        self._assert_reflects_canonical_decimal('Decimal(18, 4)', 18, 4)
+
+    def test_reflect_decimal128_as_clickhouse_canonical_precision(self):
+        self._assert_reflects_canonical_decimal('Decimal(38, 6)', 38, 6)
+
+    def test_reflect_decimal256_as_clickhouse_canonical_precision(self):
+        self._assert_reflects_canonical_decimal('Decimal(76, 8)', 76, 8)
